43292b1381de6f65f211ab3be7ca3d5294500e6f
slipson-nimonik <slipson@nimonik.com>
2019-08-29 19:09:12 +0000

    Finish working and refactored palindrome method

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 module.exports = Phrase;
+
 // Adds `reverse` to all strings.
-String.prototype.reverse = function () {
+String.prototype.reverse = function() {
     return Array.from(this).reverse().join("");
 }
 
@@ -10,8 +11,15 @@ function Phrase(content) {
     
     // Returns content processed for palindrome testing.
     this.processedContent = function processedContent() {
-        return this.content.toLowerCase();
+        return this.letters().toLowerCase();
     }
+
+    // Returns the letters in the content.
+    // For example:
+    //    new Phrase("Hello, world!").letters() === "Helloworld"
+    this.letters = function letters() {
+        return (this.content.match(/[a-z]/gi) || []).join("");
+        }
 
 // Returns true if the phrase is a palindrome, false otherwise.
 this.palindrome = function palindrome() {
